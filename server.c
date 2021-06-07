86e25284904bed56e450e42c6003c63dbb252f8c
Yegeurina <yegeurinasec@gmail.com>
2021-06-07 21:44:41 +0900

    fix error

--- server.c
+++ server.c
@@ -36,6 +36,7 @@ struct tm tm;
 
 void *thread_function(void *arg) { //명령어를 처리할 스레드
 	int i,j;
+	fd_set read_fds;	//읽기를 감지할 fd_set 구조체
 	printf("명령어 목록 : help, num_user, num_chat, ip_list,notic,exit\n");
 	while (1) {
 		char bufmsg[MAXLINE + 1];
@@ -69,7 +70,7 @@ void *thread_function(void *arg) { //명령어를 처리할 스레드
 						send(clisock_list[i], bufmsg, strlen(bufmsg), 0);
 					printf("\033[0G");		//커서의 X좌표를 0으로 이동
 					fprintf(stderr, "\033[97m");//글자색을 흰색으로 변경
-					printf("%s", buf);			//메시지 출력
+					printf("%s", bufmsg);			//메시지 출력
 					fprintf(stderr, "\033[32m");//글자색을 녹색으로 변경
 					fprintf(stderr, "server>"); //커서 출력
 				}
